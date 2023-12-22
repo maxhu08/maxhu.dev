e6a850ff9c1e042869c19abbc38c9e14cb951db8
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2023-12-22 15:09:40 -0600

    add animations and organize

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
 import { NextPage } from "next"
 import { Technologies } from "~/components/technologies"
+import styles from "~/app/page.module.scss"
+import { cn } from "~/utils/cn"
 
 const Page: NextPage = () => {
   return (
     <div className="w-full h-screen grid place-items-center">
       <div className="grid place-items-center grid-flow-row pb-16 h-max gap-2">
-        <div className="grid grid-cols-[repeat(3,max-content)] place-items-center gap-2 fade-in-down">
-          <span className="text-2xl md:text-4xl select-none wave-animation">👋</span>
+        <div className={cn("grid grid-cols-[repeat(3,max-content)] place-items-center gap-2", styles["fade-in-down"])}>
+          <span className={cn("text-2xl md:text-4xl select-none", styles["wave-animation"])} style={{ animationDelay: "1200ms" }}>
+            👋
+          </span>
           <span className="text-2xl md:text-4xl font-semibold dark:text-white">
             Hi! I&apos;m <span className="special-text">Max Hu</span>
           </span>
         </div>
         <div className="grid place-items-center grid-flow-row gap-1">
-          <span className="text-zinc-500 dark:text-zinc-400 fade-in-left ">technologies i&apos;m learning</span>
+          <span className={cn("text-zinc-500 dark:text-zinc-400k !delay-300", styles["fade-in-left"])}>technologies i&apos;m learning</span>
           <Technologies />
         </div>
       </div>
