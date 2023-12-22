c5e470b1af0cfd76cb534b550883575a493fc6b9
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2023-12-22 13:37:50 -0600

    add theme switcher

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,8 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={(cn(fira.className), "bg-neutral-900")} dir="ltr">
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+      <body className={(cn(fira.className), "bg-neutral-200 dark:bg-neutral-900")} dir="ltr">
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           <Navbar />
           {children}
         </ThemeProvider>
