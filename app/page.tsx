2b9ef84e2868538f3b5d321ff3d13667c95d76b8
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2023-12-22 14:47:54 -0600

    add animations and styling

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ const Page: NextPage = () => {
   return (
     <div className="w-full h-screen grid place-items-center">
       <div className="grid place-items-center grid-flow-row pb-16 h-max gap-2">
-        <div className="grid grid-cols-[max-content_max-content] place-items-center gap-2">
-          <span className="text-4xl select-none wave-animation">👋</span>
-          <span className="text-4xl font-semibold dark:text-white">
-            Hi! I&apos;m <span>Max Hu</span>
+        <div className="grid grid-cols-[repeat(3,max-content)] place-items-center gap-2 fade-in-down">
+          <span className="text-2xl md:text-4xl select-none wave-animation">👋</span>
+          <span className="text-2xl md:text-4xl font-semibold dark:text-white">
+            Hi! I&apos;m <span className="special-text">Max Hu</span>
           </span>
         </div>
         <div className="grid place-items-center grid-flow-row gap-1">
-          <span className="text-zinc-500 dark:text-zinc-400">technologies i&apos;m learning</span>
+          <span className="text-zinc-500 dark:text-zinc-400 fade-in-left ">technologies i&apos;m learning</span>
           <Technologies />
         </div>
       </div>
--- components/icon.tsx
+++ components/icon.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image"
 import { FC } from "react"
 
-export interface IconProps {
+export interface IconProps extends React.HTMLAttributes<HTMLDivElement> {
   name: string
   icon: string
 }
 
-export const Icon: FC<IconProps> = ({ name, icon }) => {
+export const Icon: FC<IconProps> = ({ name, icon, ...props }) => {
   return (
-    <div className="w-8 h-8 relative">
+    <div className="w-8 h-8 relative" {...props}>
       <Image src={icon} alt={name} fill />
     </div>
   )
