fa091be1674fcaf6119ea0423ede555c04b00522
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2023-12-22 17:22:18 -0600

    Update contact-me-button.tsx

--- components/contact-me-button.tsx
+++ components/contact-me-button.tsx
@@ -2,6 +2,7 @@
 
 import { ChevronRight } from "lucide-react"
 import { FC } from "react"
+import { ActionTooltip } from "~/components/action-tooltip"
 import styles from "~/components/contact-me-button.module.scss"
 import { cn } from "~/utils/cn"
 
@@ -13,11 +14,13 @@ export const ContactMeButton: FC = () => {
   }
 
   return (
-    <button onClick={openEmail} className={styles.button}>
-      <div className="text-white grid grid-cols-[max-content_max-content] place-items-center gap-1 bg-emerald-500 hover:bg-emerald-700 duration-300 ease-in-out p-2 rounded-md">
-        <ChevronRight className={cn("w-4 h-4", styles.arrow)} />
-        <span>contact me</span>
-      </div>
-    </button>
+    <ActionTooltip label="email hello@maxhu.dev" side="top">
+      <button onClick={openEmail} className={styles.button}>
+        <div className="text-white grid grid-cols-[max-content_max-content] place-items-center gap-1 bg-emerald-500 hover:bg-emerald-700 duration-300 ease-in-out p-2 rounded-md">
+          <ChevronRight className={cn("w-4 h-4", styles.arrow)} />
+          <span>contact me</span>
+        </div>
+      </button>
+    </ActionTooltip>
   )
 }
