0bf6323a48df6e06c510bff944a76fd776af6d70
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2023-12-22 13:37:50 -0600

    add theme switcher

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ import { ThemeToggle } from "~/components/theme-toggle"
 export const Navbar: FC = () => {
   return (
     <nav className="fixed z-20 w-full">
-      <div className="p-2 grid grid-flow-col w-full">
+      <div className="py-2 px-4 grid grid-flow-col w-full">
         <div className="grid grid-cols-[max-content_max-content] gap-1 text-blue-500 place-items-center select-none">
           <Zap className="w-4 h-4" />
           <span>maxhu.dev</span>
--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,5 +1,30 @@
+"use client"
+
+import { SunMoon } from "lucide-react"
+import { useTheme } from "next-themes"
 import { FC } from "react"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "~/components/ui/dropdown-menu"
 
 export const ThemeToggle: FC = () => {
-  return <div>theme toggle</div>
+  const { setTheme } = useTheme()
+
+  return (
+    <div>
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <div>
+            <SunMoon className="w-6 h-6" />
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={() => setTheme("light")} className="cursor-pointer">
+            <div>click</div>
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("dark")} className="cursor-pointer">
+            <div>click</div>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  )
 }
