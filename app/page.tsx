88dbab510a184140883660bbfb1c689880ce2acf
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2023-12-22 12:51:32 -0600

    basic styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
-import { NextPage } from "next";
+import { NextPage } from "next"
 
 const Page: NextPage = () => {
-  return <div>hi</div>;
-};
+  return (
+    <div className="w-full h-screen">
+      <div>
+        <div>
+          <div className="grid grid-cols-[max-content_max-content] place-items-center gap-2">
+            <span className="text-4xl">👋</span>
+            <span className="text-4xl font-semibold">
+              Hi! I&apos;m <span>Max Hu</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
-export default Page;
+export default Page
