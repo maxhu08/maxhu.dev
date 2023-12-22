b1c097ac3d950a4d7d17b9f95b793416323fc179
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2023-12-22 15:21:51 -0600

    add more light dark support

--- components/technologies.tsx
+++ components/technologies.tsx
@@ -1,12 +1,12 @@
 import { FC } from "react"
-import { Icon, IconProps } from "~/components/technology-icon"
+import { Icon } from "~/components/technology-icon"
 
 const items = [
   { name: "javascript", icon: "/assets/technologies/javascript.svg" },
   { name: "typescript", icon: "/assets/technologies/typescript.svg" },
   { name: "nodejs", icon: "/assets/technologies/nodejs.svg" },
   { name: "react", icon: "/assets/technologies/react.svg" },
-  { name: "nextjs", icon: "/assets/technologies/nextjs.svg" },
+  { name: "nextjs", icon: "/assets/technologies/nextjs_dark.svg", iconDark: "/assets/technologies/nextjs_light.svg" },
   { name: "tailwindcss", icon: "/assets/technologies/tailwindcss.svg" },
   { name: "prisma", icon: "/assets/technologies/prisma.svg" }
 ]
@@ -15,7 +15,7 @@ export const Technologies: FC = () => {
   return (
     <div className="p-2 grid grid-flow-col place-items-center gap-2">
       {items.map((item, index) => (
-        <Icon name={item.name} icon={item.icon} key={item.name} delay={index * 100 + 600} />
+        <Icon name={item.name} icon={item.icon} iconLight={item.iconDark} key={item.name} delay={index * 100 + 600} />
       ))}
     </div>
   )
--- components/technology-icon.tsx
+++ components/technology-icon.tsx
@@ -1,18 +1,31 @@
+"use client"
+
 import Image from "next/image"
-import { FC } from "react"
+import { FC, useEffect, useState } from "react"
 import { cn } from "~/utils/cn"
 import styles from "./technology-icon.module.scss"
+import { useTheme } from "next-themes"
 
 export interface IconProps extends React.HTMLAttributes<HTMLDivElement> {
   name: string
   icon: string
+  iconLight?: string
   delay: number
 }
 
-export const Icon: FC<IconProps> = ({ name, icon, delay, ...props }) => {
+export const Icon: FC<IconProps> = ({ name, icon, iconLight, delay, ...props }) => {
+  const { theme } = useTheme()
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) return null
+
   return (
     <div className={cn("w-8 h-8 relative", styles["fade-in-right"])} style={{ animationDelay: `${delay.toString()}ms !important` }} {...props}>
-      <Image src={icon} alt={name} fill />
+      {iconLight && theme === "light" ? <Image src={iconLight} alt={name} fill /> : <Image src={icon} alt={name} fill />}
     </div>
   )
 }
