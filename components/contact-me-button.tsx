38b2e6c11405891ed7f3558158013e28b771d4ba
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2023-12-23 11:36:32 -0600

    add projects button

--- components/contact-me-button.tsx
+++ components/contact-me-button.tsx
@@ -14,7 +14,7 @@ export const ContactMeButton: FC = () => {
   }
 
   return (
-    <ActionTooltip label="email hello@maxhu.dev" side="top">
+    <ActionTooltip label="email hello@maxhu.dev" side="bottom">
       <button onClick={openEmail} className={styles.button}>
         <div className="text-white grid grid-cols-[max-content_max-content] place-items-center gap-1 bg-emerald-500 hover:bg-emerald-700 duration-300 ease-in-out p-2 rounded-md">
           <ChevronRight className={cn("w-4 h-4", styles.arrow)} />
