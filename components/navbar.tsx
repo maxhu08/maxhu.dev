2c0fe6e9e8229fc0d60ebc4d62f029e7978e7074
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2023-12-23 13:45:58 -0600

    improve navbar and more

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,52 @@
-import { Zap } from "lucide-react"
+"use client"
+
+import { ChevronLeft, Webhook, Zap } from "lucide-react"
+import { usePathname, useRouter } from "next/navigation"
 import { FC } from "react"
 import { ThemeToggle } from "~/components/theme-toggle"
 
 export const Navbar: FC = () => {
-  return (
-    <nav className="fixed z-20 w-full top-0 ease-in-out backdrop-blur-lg">
-      <div className="py-2 px-4 grid grid-flow-col w-full">
+  const pathname = usePathname()
+  const router = useRouter()
+
+  const navigationControls = () => {
+    if (pathname === "/") {
+      return (
         <div className="grid grid-cols-[max-content_max-content] gap-1 text-blue-500 place-items-center select-none">
           <Zap className="w-4 h-4" />
           <span>maxhu.dev</span>
         </div>
+      )
+    } else {
+      return (
+        <button onClick={() => router.back()} className="cursor-pointer">
+          <div className="grid grid-cols-[max-content_max-content] gap-1 text-blue-500 hover:text-blue-700 duration-300 ease-in-out place-items-center cursor-pointer">
+            <ChevronLeft className="w-4 h-4" />
+            <span>Back</span>
+          </div>
+        </button>
+      )
+    }
+  }
+
+  const currentPage = () => {
+    if (pathname === "/") {
+      return <div></div>
+    } else if (pathname === "/projects") {
+      return (
+        <div className="grid grid-cols-[max-content_max-content] gap-1 place-items-center">
+          <Webhook className="w-4 h-4" />
+          <span>projects</span>
+        </div>
+      )
+    }
+  }
+
+  return (
+    <nav className="fixed z-20 w-full top-0 ease-in-out backdrop-blur-lg">
+      <div className="py-2 px-4 grid grid-cols-[1fr_auto_1fr] w-full">
+        <div className="w-max">{navigationControls()}</div>
+        {currentPage()}
         <div className="ml-auto">
           <ThemeToggle />
         </div>
