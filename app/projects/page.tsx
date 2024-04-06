5a58378ab2317f06bfeb1d6d529816c8a51751ca
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-04-06 12:20:45 -0500

    more projects page stuff

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -8,16 +8,20 @@ const Page: NextPage = () => {
       <div className="grid grid-flow-row md:grid-cols-2 gap-2">
         <ProjectCard
           info={{
-            title: "tomb-of-the-mask-clone",
-            technologies: ["java"],
-            link: "https://github.com/maxhu08/tomb-of-the-mask-clone",
+            title: "mtab",
+            description: "simple new tab page extension for chrome",
+            technologies: ["html", "css", "typescript"],
+            demoLink:
+              "https://chromewebstore.google.com/detail/mtab/fdaphilojaklgkoocegabckfanjoacjg",
+            codeLink: "https://github.com/maxhu08/mtab",
           }}
         />
         <ProjectCard
           info={{
-            title: "mtab",
-            technologies: ["html", "css", "typescript"],
-            link: "https://github.com/maxhu08/mtab",
+            title: "tomb-of-the-mask-clone",
+            description: "simple version tomb of the mask written in java",
+            technologies: ["java"],
+            codeLink: "https://github.com/maxhu08/tomb-of-the-mask-clone",
           }}
         />
       </div>
--- components/projects/project-card.tsx
+++ components/projects/project-card.tsx
@@ -5,12 +5,16 @@ import { ActionTooltip } from "~/components/action-tooltip";
 import Image from "next/image";
 import { useTheme } from "next-themes";
 import { technologies } from "~/constants/technologies";
+import { Paperclip, Code2 } from "lucide-react";
+import Link from "next/link";
 
 interface ProjectCardProps {
   info: {
     title: string;
+    description: string;
     technologies: string[];
-    link?: string;
+    codeLink?: string;
+    demoLink?: string;
   };
 }
 
@@ -18,29 +22,61 @@ export const ProjectCard: FC<ProjectCardProps> = ({ info }) => {
   const { theme } = useTheme();
 
   return (
-    <div className="bg-neutral-200 dark:bg-neutral-900 p-4 rounded-md border-zinc-400 dark:border-border border-[1px]">
-      <p className="font-semibold text-cyan-500">{info.title}</p>
-      <div className="grid grid-flow-col w-max gap-1">
-        {technologies.map((technology, index: number) => {
-          if (info.technologies.includes(technology.name)) {
-            return (
-              <ActionTooltip label={technology.name} side="bottom" key={index}>
-                <div className="h-6 relative aspect-square">
-                  {technology.iconLight && theme === "light" ? (
-                    <Image
-                      src={technology.iconLight}
-                      alt={technology.name}
-                      fill
-                    />
-                  ) : (
-                    <Image src={technology.icon} alt={technology.name} fill />
-                  )}
+    <Link href={`/projects/${info.title}`} className="cursor-pointer">
+      <div className="relative rounded-md overflow-hidden border-zinc-400 dark:border-border border-[1px]">
+        <div className="hover:bg-black/20 dark:hover:bg-white/20 w-full h-full absolute duration-300 ease-in-out"></div>
+        <div className="bg-neutral-200 dark:bg-neutral-900 p-2 grid grid-flow-row">
+          <p className="font-semibold text-cyan-500">{info.title}</p>
+          <div className="grid grid-flow-col w-max gap-1">
+            {technologies.map((technology, index: number) => {
+              if (info.technologies.includes(technology.name)) {
+                return (
+                  <ActionTooltip
+                    label={technology.name}
+                    side="bottom"
+                    key={index}
+                  >
+                    <div className="h-6 relative aspect-square">
+                      {technology.iconLight && theme === "light" ? (
+                        <Image
+                          src={technology.iconLight}
+                          alt={technology.name}
+                          fill
+                        />
+                      ) : (
+                        <Image
+                          src={technology.icon}
+                          alt={technology.name}
+                          fill
+                        />
+                      )}
+                    </div>
+                  </ActionTooltip>
+                );
+              }
+            })}
+          </div>
+          <p className="pt-2">{info.description}</p>
+          <div className="pt-2 grid grid-flow-col gap-4 w-max">
+            {info.demoLink && (
+              <a href={info.demoLink} target="_blank">
+                <div className="grid grid-cols-[max-content_max-content] place-items-center gap-1 text-blue-500 hover:text-blue-600 duration-300 ease-in-out">
+                  <Paperclip className="w-4 h-4" />
+                  <span>View demo</span>
                 </div>
-              </ActionTooltip>
-            );
-          }
-        })}
+              </a>
+            )}
+            {info.codeLink && (
+              <a href={info.codeLink} target="_blank">
+                <div className="grid grid-cols-[max-content_max-content] place-items-center gap-1 text-blue-500 hover:text-blue-600 duration-300 ease-in-out">
+                  <Code2 className="w-4 h-4" />
+                  <span>View code</span>
+                </div>
+              </a>
+            )}
+          </div>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
