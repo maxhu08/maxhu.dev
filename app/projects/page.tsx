bda9df4ad1332d19f6338884f3bfa3ec3bf96fd2
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-04-28 21:19:02 -0500

    fix breaking deployment

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,14 +1,13 @@
 import { NextPage } from "next";
 import { ProjectCard } from "~/components/projects/project-card";
-import { projects } from "~/constants/projects";
+import { ProjectName, projects } from "~/constants/projects";
 
 const Page: NextPage = () => {
   return (
     <main className="grid grid-flow-row mx-auto mt-20 mb-20 md:mb-60 w-[95%] lg:w-[75%]">
       <div className="grid grid-flow-row md:grid-cols-2 xl:grid-cols-4 gap-2">
         {Object.keys(projects).map((key, index) => {
-          // @ts-expect-error
-          const project = projects[key];
+          const project = projects[key as ProjectName];
 
           return (
             <ProjectCard
