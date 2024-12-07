ede5776b782b1e312341701ea3eb48205678ccd2
maxhu08 <contact@maxhu.dev>
2024-12-07 14:18:36 -0600

    add redirect

--- constants/redirects.ts
+++ constants/redirects.ts
@@ -1,5 +1,8 @@
 export const redirects = {
   github: "https://github.com/maxhu08",
   youtube: "https://www.youtube.com/@maxhudotdev",
-  twitter: "https://twitter.com/maxhu08"
+  twitter: "https://twitter.com/maxhu08",
+  discord: {
+    mtab: "https://discord.gg/JMrDphV5xp"
+  }
 };
